d1e957f23a2098982a7b38aae5d5aa30dbf2a76c
prashuk jain <29jainprashuk@gmail.com>
2024-01-12 09:25:09 +0530

    advanced js

---
+++ class.js
@@ -0,0 +1,9 @@
+let obj = {
+    FirstName : "Shivam",
+    LastName : "kumar",
+    Address : "Delhi"
+}
+
+
+const {FirstName , LastName , Address} = obj ;
+console.log(FirstName,LastName ,Address);
